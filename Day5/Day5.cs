589b9838122a39266dcee210575021d8b2e4b0a7
kloweemalakowsky <klowee.malakowsky@integrate.com>
2022-12-14 16:05:24 -0600

    cleaned up day 5 a little

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -11,44 +11,26 @@ public class Day5
         
         List<int> topCrates = new();
         
-        
         char[] arr1 = new char[]{'F', 'C', 'F', 'P', 'H', 'T', 'W' };
-        Stack<char> stack1 = new Stack<char>(arr1);
- 
         char[] arr2 = new char[]{'G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H' };
-        Stack<char> stack2 = new Stack<char>(arr2);
-
         char[] arr3 = new char[]{'H', 'P', 'T', 'R' };
-        Stack<char> stack3 = new Stack<char>(arr3);
-        
         char[] arr4 = new char[]{'Z', 'S', 'N', 'P', 'H', 'T' };
-        Stack<char> stack4 = new Stack<char>(arr4);
-        
         char[] arr5 = new char[]{'N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D' };
-        Stack<char> stack5 = new Stack<char>(arr5);
-        
         char[] arr6 = new char[]{'P', 'M', 'G', 'F', 'W', 'D', 'Z' };
-        Stack<char> stack6 = new Stack<char>(arr6);
-        
         char[] arr7 = new char[]{'M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P' };
-        Stack<char> stack7 = new Stack<char>(arr7);
-        
         char[] arr8 = new char[]{'N', 'D', 'S' };
-        Stack<char> stack8 = new Stack<char>(arr8);
-        
         char[] arr9 = new char[]{'D', 'Z', 'S', 'F', 'M' };
-        Stack<char> stack9 = new Stack<char>(arr9);
-
-        myStacks.Add(stack1); //index 0 
-        myStacks.Add(stack1);
-        myStacks.Add(stack2);
-        myStacks.Add(stack3);
-        myStacks.Add(stack4);
-        myStacks.Add(stack5);
-        myStacks.Add(stack6);
-        myStacks.Add(stack7);
-        myStacks.Add(stack8);
-        myStacks.Add(stack9);
+        myStacks.Add(new Stack<char>()); //index 0 Don't use
+        
+        myStacks.Add(new Stack<char>(arr1));
+        myStacks.Add(new Stack<char>(arr2));
+        myStacks.Add(new Stack<char>(arr3));
+        myStacks.Add(new Stack<char>(arr4));
+        myStacks.Add(new Stack<char>(arr5));
+        myStacks.Add(new Stack<char>(arr6));
+        myStacks.Add(new Stack<char>(arr7));
+        myStacks.Add(new Stack<char>(arr8));
+        myStacks.Add(new Stack<char>(arr9));
         
         
         foreach (var line in text)
